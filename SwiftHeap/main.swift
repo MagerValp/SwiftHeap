adee6bc619651e927816c5110e24494d91360928
Per Olofsson <MagerValp@gmail.com>
2014-06-21 09:26:21 +0200

    Initial Commit

---
+++ SwiftHeap/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  SwiftHeap
+//
+//  Created by Pelle on 2014-06-21.
+//  Copyright (c) 2014 AutoMac. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
